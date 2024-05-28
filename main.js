ec089c4dbe91163adf120b8382edd7ca2af44aae
MykolaDekusar <ndekusar97@gmail.com>
2024-05-29 00:23:00 +0200

    Main exercise completed

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 "use strict";
+const uList = document.getElementById("mails");
+const mailList = [];
+for (let i = 0; i < 10; i++) {
+  //implementing axios
+  axios
+    .get("https://flynn.boolean.careers/exercises/api/random/mail")
+    .then((info) => {
+      //adding random email to list
+      mailList.push(info.data.response);
+    });
+}
+
+for (let i = 0; i < 10; i++) {
+  uList.innerHTML += `<li>${mailList}</li>`;
+  console.log(mailList[i]);
+}
